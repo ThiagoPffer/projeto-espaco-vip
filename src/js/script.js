42cce080210f1bd29ff67c27ee10136905524506
Thiago Piffer Ramos <thiago.ramos@betha.com.br>
2022-03-08 14:44:27 -0300

    logo / cor da moldura / permitir selecao de multiplas imagens / baixar em jpeg

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,7 @@
 const pictureInput = document.getElementById('photo-file');
 const selectImageBtn = document.getElementById('select-image');
 const downloadImageBtn = document.getElementById('download-image');
-const canvasPreview = document.getElementById('canvas-preview');
-const canvasContext = canvasPreview.getContext('2d');
+const canvasList = [];
 
 selectImageBtn.onclick = function() {
     pictureInput.click();
@@ -10,43 +9,64 @@ selectImageBtn.onclick = function() {
 
 window.addEventListener('DOMContentLoaded', () => {
     pictureInput.addEventListener('change', () => {
-        let pictureFile = pictureInput.files.item(0);
-        let reader = new FileReader();
-        
-        reader.readAsDataURL(pictureFile);
-        reader.onload = function(event) {
-            var base_image = new Image();
-            base_image.src = event.target.result;
-            base_image.onload = function(){
-                canvasPreview.setAttribute('width', base_image.width);
-                canvasPreview.setAttribute('height', base_image.height);
-                canvasContext.drawImage(base_image, 0, 0);
-                canvasContext.lineWidth = 16;
-                canvasContext.strokeStyle = '#f3b60d';
-                canvasContext.strokeRect(0, 0, base_image.width, base_image.height);
-                
-                var logo = new Image();
-                logo.src = 'src/img/logo-menor.png';
-                logo.onload = function() {
-                    canvasContext.beginPath();
-                    canvasContext.drawImage(logo, base_image.width - 150, base_image.height - 164);
-                }
-
-            }
-            downloadImageBtn.classList.remove('d-none');
+        var pictureFiles = [];
+        for (let i = 0; i < pictureInput.files.length; i++) {
+            pictureFiles.push(pictureInput.files.item(i));
         }
+        pictureFiles.forEach((pictureFile, index) => {
+            createNewCanvasElement(index);
+            let reader = new FileReader();
+            reader.readAsDataURL(pictureFile);
+            reader.onload = event => {
+                var baseImage = new Image();
+                baseImage.src = event.target.result;
+                baseImage.onload = () => {
+                    drawImage(baseImage, index);
+                }
+                downloadImageBtn.classList.remove('d-none');
+            }
+            console.log('reading');
+        })
     });
 });
 
+function createNewCanvasElement(index) {
+    let canvasContainer = document.getElementById('canvas-container');
+    let newCanvas = document.createElement('canvas');
+    newCanvas.setAttribute('id', `canvas-preview-${index}`);
+    newCanvas.setAttribute('class', 'canvas-preview');
+    canvasContainer.appendChild(newCanvas);
+}
+
+function drawImage(baseImage, index) {
+    let canvasPreview = document.getElementById(`canvas-preview-${index}`);
+    let canvasContext = canvasPreview.getContext('2d');
+    canvasPreview.setAttribute('width', baseImage.width);
+    canvasPreview.setAttribute('height', baseImage.height);
+    canvasContext.drawImage(baseImage, 0, 0);
+    canvasContext.lineWidth = 16;
+    canvasContext.strokeStyle = '#FED65A';
+    canvasContext.strokeRect(0, 0, baseImage.width, baseImage.height);
+    drawLogo(baseImage, canvasContext, canvasPreview);
+}
+
+function drawLogo(baseImage, canvasContext, canvasPreview) {
+    var logo = new Image();
+    logo.src = 'src/img/logo-menor.png';
+    logo.onload = () => {
+        canvasContext.beginPath();
+        canvasContext.drawImage(logo, baseImage.width - 150, baseImage.height - 166);
+        canvasList.push(canvasPreview);
+    }
+}
+
 downloadImageBtn.onclick = function() {
-    if (window.navigator.msSaveBlob) {
-        window.navigator.msSaveBlob(canvasPreview.msToBlob(), "canvas-image.png");
-    } else {
+    canvasList.forEach(canvasPreview => {
         const link = document.createElement("a");
         document.body.appendChild(link);
-        link.href = canvasPreview.toDataURL();
-        link.download = "canvas-image.png";
+        link.href = canvasPreview.toDataURL("image/jpeg");
+        link.download = "canvas-image.jpeg";
         link.click();
         document.body.removeChild(link);
-    }
+    });
 }
